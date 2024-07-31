a0adbd47bcd4a532f576eafdf5087f64014bd4f4
mengyife <yoengmengyi@gmail.com>
2024-07-31 08:18:08 +0000

    init code

---
+++ pages/link/_id.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Deep Link Page</h1>
+    <p>Link ID: {{ id }}</p>
+  </div>
+</template>
+
+<script setup>
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+const id = route.params.id;
+</script>
